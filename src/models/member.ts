0605798a58472ce21d09be349cf711f62e23dfc3
msaad53407 <msmuhammadsaad78@gmail.com>
2024-07-01 16:08:55 +0500

    Add userId field to member collection

--- src/models/member.ts
+++ src/models/member.ts
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const memberSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     greeting: {
       type: String,
       required: true,
