bd7aed3c5105bd7812016d7bdcbf590bf0ac90f7
Muhammad Saad <msmuhammadsaad78@gmail.com>
2024-08-21 21:40:54 +0500

    Implement custom sign in flow to validate role

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { isAuthenticated } from "@/lib/authorization";
-import { clerkClient } from "@clerk/nextjs/server";
+import { Roles } from "@/types/globals";
+import { clerkClient, User } from "@clerk/nextjs/server";
 
 export const getUser = async (userId?: string) => {
   if (!(await isAuthenticated())) {
@@ -32,3 +33,52 @@ export const getUser = async (userId?: string) => {
     message: "User fetched Successfully",
   };
 };
+
+export async function validateRole(email?: string, role?: Roles) {
+  try {
+    if (!email || !role) {
+      return {
+        message: "Missing email or role",
+        data: null,
+      };
+    }
+
+    const user = await clerkClient().users.getUserList({
+      emailAddress: [email],
+    });
+
+    if (user.totalCount === 0) {
+      return {
+        message: "User not found",
+        data: null,
+      };
+    }
+
+    const userRole = user.data[0].publicMetadata?.role as Roles | undefined;
+
+    if (!userRole) {
+      return {
+        message: "User role not found",
+        data: null,
+      };
+    }
+
+    if (userRole !== role) {
+      return {
+        message: "User role does not match with the provided role",
+        data: null,
+      };
+    }
+
+    return {
+      message: "Member role validated",
+      data: JSON.parse(JSON.stringify(user.data[0])) as User,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Internal server error. Please try again later.",
+      data: null,
+    };
+  }
+}
