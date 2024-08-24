3c79e957f001f4b6c8c0d67b54648230ef75eee0
Muhammad Saad <msmuhammadsaad78@gmail.com>
2024-08-24 18:17:07 +0500

    Update dues row in table to show latest data

--- src/app/(ui)/(protected)/member/[memberId]/dues/components/DuesRow.tsx
+++ src/app/(ui)/(protected)/member/[memberId]/dues/components/DuesRow.tsx
@@ -46,11 +46,11 @@ const DuesRow = ({
       ? new Date(dues.dueDate).toISOString().split("T")[0]
       : "",
     balanceForward:
-      new Date(dues.dueDate).getMonth() < new Date().getMonth() + 1
+      new Date(dues.dueDate).getMonth() < new Date().getMonth()
         ? dues.balanceForward!
         : duesLeftForYear,
     memberBalance:
-      new Date(dues.dueDate).getMonth() < new Date().getMonth() + 1
+      new Date(dues.dueDate).getMonth() < new Date().getMonth()
         ? dues.memberBalance!
         : extraDues,
   });
@@ -58,11 +58,11 @@ const DuesRow = ({
     setFormFields({
       ...formFields,
       balanceForward:
-        new Date(dues.dueDate).getMonth() < new Date().getMonth() + 1
+        new Date(dues.dueDate).getMonth() < new Date().getMonth()
           ? dues.balanceForward!
           : duesLeftForYear,
       memberBalance:
-        new Date(dues.dueDate).getMonth() < new Date().getMonth() + 1
+        new Date(dues.dueDate).getMonth() < new Date().getMonth()
           ? dues.memberBalance!
           : extraDues,
     });
